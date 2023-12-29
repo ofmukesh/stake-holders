2fd786e102ce2dcc781305546891122fb59791a4
Marco <of.mukesh@gmail.com>
2023-12-29 22:11:40 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import shapeImage from "./shape.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const shapes = [{}, {}, {}, {}, {}, {}];
+  const circleWidth = 600; // in px
+  const shapeExtraWidth = 120;
+  const shapeWidth = circleWidth / shapes.length + shapeExtraWidth;
+
+  useEffect(() => {
+    var items = document.getElementsByClassName("item");
+    var angle = 360 / items.length;
+    for (var i = 0; i < items.length; i++) {
+      items[i].style.transform = `rotate(${angle * i}deg)`;
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        className="main"
+        style={{ width: circleWidth + "px", height: circleWidth + "px" }}
+      >
+        {shapes.map((item, idx) => {
+          return (
+            <div key={idx} className="item">
+              <img src={shapeImage} width={shapeWidth} height={shapeWidth} />
+              <p className="item-text">Hlo</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
