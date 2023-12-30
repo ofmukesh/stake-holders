538a3e5cc26c11e631f01feb0249edbbcc7dbf32
Marco <of.mukesh@gmail.com>
2023-12-30 18:25:32 +0530

    final

--- src/stakeHolder.jsx
+++ src/stakeHolder.jsx
@@ -1,45 +1,56 @@
 import React, { useEffect } from "react";
 import shapeImage from "../src/shape.svg";
 
+const shapeWidths = {
+  3: 150,
+  4: 110,
+  5: 230,
+  6: 220,
+  7: 230,
+  8: 240,
+};
+
 function StakeHolder({ entries }) {
-  const shapes = entries;
-  const circleWidth = 1000; // in px
-  const shapeExtraWidth = 220;
-  const shapeWidth = circleWidth / shapes.length + shapeExtraWidth;
+  const circleWidth = 1000;
+  const shapeExtraWidth = shapeWidths[entries.length];
+  const shapeWidth = circleWidth / entries.length + shapeExtraWidth;
 
   useEffect(() => {
     var items = document.getElementsByClassName("item");
-    // var icons = document.getElementsByClassName("item-icon");
     var angle = 360 / items.length;
     for (var i = 0; i < items.length; i++) {
       items[i].style.transform = `rotate(${angle * i}deg)`;
-    //   icons[i].style.transform = `rotate(${angle * i}deg)`;
     }
   }, []);
+
   return (
     <div
       className="main"
       style={{ width: circleWidth + "px", height: circleWidth + "px" }}
     >
-      {shapes.map((item, idx) => {
+      {entries.map((item, idx) => {
         return (
           <div key={idx} className="item">
-            <img src={shapeImage} width={shapeWidth} height={shapeWidth} />
-            <i
-              key={idx}
-              className={` fa-solid fa-3x ${item.icon}`}
-            ></i>
+            <div
+              className="image-div"
+              style={{ width: shapeWidth + "px", height: shapeWidth + "px" }}
+            >
+              <img
+                src={shapeImage}
+                alt={`shape-${idx}`}
+                width={shapeWidth}
+                height={shapeWidth}
+              />
+              <div className="image-text">
+                <b>{item.role}</b>
+                <br />
+                {item.description}
+              </div>
+              <i className={`fa-solid fa-3x ${item.icon} item-icon`}></i>
+            </div>
           </div>
         );
       })}
-      {/* <div className="icons">
-        {shapes.map((item, idx) => {
-          return (
-            <i key={idx} className={`item-icon fa-solid fa-3x ${item.icon}`} >
-            </i>
-          );
-        })}
-      </div> */}
     </div>
   );
 }
